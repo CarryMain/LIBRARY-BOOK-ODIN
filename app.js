f18300ececc45b2fcbeac4ee8607df473b4c1252
CarryMain <mr.turanov98@gmail.com>
2023-12-06 12:09:39 +0300

    end logic and style

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const myLibrary = [{title: 'Harry', author: 'Rouling', pages: 234, read: 'No'}];
+const myLibrary = [];
 
 class Book {
     constructor(title, author, pages, read) {
@@ -9,42 +9,77 @@ class Book {
     }
 }
 
+const   titleF = document.getElementById('title'),
+        authorF = document.getElementById('author'),
+        pagesF = document.getElementById('number'),
+        readF = document.getElementById('read');
+
 const onBtn = document.querySelector('#onBtn');
 const modal  = document.querySelector('.modal');
 onBtn.addEventListener('click', () => {
     modal.classList.toggle('hidden');
 })
 
-
-
 function addBook() {
-    const title = document.getElementById('title'),
-          author = document.getElementById('author'),
-          pages = document.getElementById('number'),
-          read = document.getElementById('read');
-    const book = new Book(title.value, author.value, pages.value,`${read.checkbox ? 'Yes' : 'No'}` )
-    myLibrary.push(book);
-    return myLibrary;
+    let title = titleF.value;
+    let author = authorF.value;
+    let pages = pagesF.value;
+    let read = getRead();
+    const newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
 }
 
-function addingBookToLibrary() {
-    const btnModal = document.querySelector('#modalBtn');
-    const div = document.createElement('div');
+
+function getRead() {
+    return readF.value === 'reading';
+}
+
+function displayBooks() {
     const left = document.querySelector('.left');
-    btnModal.addEventListener('click', () => {
+    left.innerHTML = '';
+    for (let i = 0; i < myLibrary.length; i++) {
+        const div = document.createElement('div');
         div.classList.add('item');
-        myLibrary.forEach(item => {
-            div.innerHTML = `
-            <p><span>Title:</span> ${item.title}</p>
-            <p><span>Author:</span> ${item.author}</p>
-            <p><span>Pages:</span> ${item.pages}</p>
-            <p><span>Read: </span> ${item.read}</p>
+        div.innerHTML = ` 
+            <p><span>Title:</span> ${myLibrary[i].title}</p>
+            <p><span>Author:</span> ${myLibrary[i].author}</p>
+            <p><span>Pages:</span> ${myLibrary[i].pages}</p>
+            <p><span>Read: </span> <span class="read-value">${myLibrary[i].read}</span></p>
+            <button class="deleteBtn" data-index="${i}">Delete</button>
+            <button class="changeReadBtn" data-index="${i}">Change Read</button>
         `;
-        })
         left.append(div);
-        addBook();
-    })
+    }
+
+    const deleteButtons = document.querySelectorAll('.deleteBtn');
+    deleteButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const index = button.dataset.index;
+            deleteBook(index);
+            displayBooks();
+        })
+    });
+
+    const changeReadButtons = document.querySelectorAll('.changeReadBtn');
+    changeReadButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const index = button.dataset.index;
+            changeRead(index);
+            displayBooks();
+        })
+    });
 }
 
+function changeRead(index) {
+    myLibrary[index].read = myLibrary[index].read === true ? false : true;
+}
 
-addingBookToLibrary();
+modalBtn.addEventListener('click', () => {
+    addBook();
+    displayBooks();
+});
+
+
+function deleteBook(index) {
+    myLibrary.splice(index, 1);
+}
